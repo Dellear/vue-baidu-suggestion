5dbb673aed380093049e86ab92c5641bda41eff3
wang <358009775@qq.com>
2017-05-20 01:40:36 +0800

    Vuex 全局state

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,34 @@
 <template>
   <div id="app">
-    <h1>{{title}}</h1>
-    <input v-model="newItem" @keyup.enter="addNew" />
-    <ul>
-      <li v-for="item in items" v-bind:class="{finished : item.isFinished}" v-on:click="toggleFinish(item)">
-        {{item.label}}
-      </li>
-      
-    </ul>
-    <p>Child tell me:{{ childWords }}</p>
-    <component-a msgFromFather="you die!" 
-      @child-tell-me="listenToMyBoy"></component-a>
+    <p>{{ count }}</p>
+    <p>
+      <button v-on:click="increment">+</button>
+      <button v-on:click="decrement">-</button>
+    </p>
+    <counter></counter>
   </div>
 </template>
 
 <script>
-  import Store from './store';
-  import ComponentA from './components/componentA';
+  import counter from './components/Counter';
+  
   export default {
-    data() {
-      return {
-        title: `this is a todo list`,
-        items: Store.fetch(),
-        newItem:'',
-        childWords:''
+    computed: {
+      count () {
+        return store.state.count;
       }
     },
-    components:{ ComponentA },
-    watch: {
-      items: {
-        handler: function (val ,oldVal) {
-          Store.save(val);
-        },
-        deep: true
-      }
-    },
+    components:{ counter },
     methods: {
-      toggleFinish: function (item) {
-      	item.isFinished = !item.isFinished;
+      increment () {
+        store.commit('increment');
       },
-      addNew: function () {
-      	this.items.unshift({
-      	  label: this.newItem,
-      	  isFinished: false
-      	});
-      	this.newItem = '';
-      },
-      listenToMyBoy (msg) {
-        this.childWords = msg;
+      decrement () {
+        store.commit('decrement');
       }
     }
   }
 </script>
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
-  }
-  .img {
-    background: url(assets/201272522441773.gif) no-repeat center;
-  }
-  .finished {
-    text-decoration: line-through;
-  }
 </style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="color:red;">{{ count }}</div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      count () {
+        return store.state.count + 55;
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
