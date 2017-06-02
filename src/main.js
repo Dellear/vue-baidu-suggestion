b5e35d6fce7518d6cccc7e3456ccbdba309b063b
wang <358009775@qq.com>
2017-06-03 02:31:38 +0800

    vue关键词联想组件完善（百度版）

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
-import App from './App';
+import BaiduSug from './components/BaiduSug';
 import VueResource from 'vue-resource';
 import './css/index.css';
 
@@ -14,6 +14,6 @@ Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  template: '<App/>',
-  components: { App }
+  template: '<BaiduSug/>',
+  components: { BaiduSug }
 })
